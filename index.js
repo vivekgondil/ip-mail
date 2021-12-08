68ce6eec0323947324c9d3565f925f6ae59bf85b
vivekg-Osmosys <vivek.g@osmosys.co>
2021-12-08 21:28:39 +0530

    Added the mailing bot

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-const publicIp = require('public-ip');
+const nodemailer = require(`nodemailer`);
+require("dotenv").config();
+const publicIp = require("public-ip");
+
+const getIpAddress = async () => {
+  return await publicIp.v4({ fallbackUrls: ["https://ifconfig.co/ip"] });
+};
+async function sendMail({ to, text, subject, html }) {
+  let transporter = nodemailer.createTransport({
+    service: `gmail`,
+    auth: {
+      user: process.env.EMAILID, // generated ethereal user
+      pass: process.env.PASSWD, // generated ethereal password
+    },
+  });
+  let info = await transporter.sendMail({
+    from: process.env.EMAILID, // sender address
+    to, // list of receivers
+    subject, // Subject line
+    text, // plain text body
+    html,
+  });
+  console.log(`Message sent: %s`, info.messageId);
+}
 
 (async () => {
-    let ip = 
-	await publicIp.v4({
-		fallbackUrls: [
-			'https://ifconfig.co/ip'
-		]
-	});
-    console.log(ip);
+  let previousIp;
+  setInterval(async () => {
+    const ip = await getIpAddress();
+    if (previousIp !== ip) {
+      console.log(`IP changed form ${previousIp} => ${ip}`);
+      previousIp = ip;
+      sendMail({
+        to: `gondilvivek@gmail.com`,
+        subject: `IP Change notification Test`,
+        text: `Your IP is changed to ${ip}`,
+      }).catch(console.error);
+    }
+  }, 60000);
 })();
